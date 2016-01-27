56656e4db2941a8eb6cf1329c14242149e6b062d
Tom Kuiper <kuiper@jpl.nasa.gov>
2016-01-27 09:58:31 -0800

    Starting on Dare subclass of Radiometer

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 from glob import glob
-from math import log
+from math import ceil, log, sqrt
+from numpy import array
 from serial import Serial
-from time import sleep
+from time import sleep, time
 import logging
 
 from Electronics.Instruments import PowerMeter
-
+from support import nearest_index
 module_logger = logging.getLogger(__name__)
+
+sqrt10 = sqrt(10)
 
 IDs = {"1.99.234.24.23.0.0.212":  0,
        "1.222.178.24.23.0.0.50":  1,
@@ -44,10 +47,10 @@ class Radipower(PowerMeter,Serial):
   ========= 
   http://dsnra.jpl.nasa.gov/dss/manuals/RadiPower_Standalone_V1_1.pdf
   """
-  averages = {1:1, 2:3, 3:10, 4:30, 5:100, 6:300, 7:1000}
+  averages = {1:10, 2:30, 3:100, 4:300, 5:1000, 6:3000, 7:5000}
   
   def __init__(self, device="/dev/ttyUSB0", baud=115200,
-               timeout=1, writeTimeout=1, num_avg=10):
+               timeout=1, writeTimeout=1):
     """
     """
     mylogger = logging.getLogger(module_logger.name+".Radipower")
@@ -61,7 +64,6 @@ class Radipower(PowerMeter,Serial):
     self._attributes_.append('logger')
     if self.get_ID():
       self.name = self.ID
-      #self.write('REBOOT SYSTEM\n')
       self._attributes_.append('ID')
       self.identify()
       self._attributes_.append('model')
@@ -72,8 +74,12 @@ class Radipower(PowerMeter,Serial):
       self.f_max = float(self.ask("FREQUENCY? MAX")[:-4])/1.e6 # GHz
       self.p_min = -55 # dBm
       self.p_max = +10 # dBm
-      self.set_averaging(num_avg) # sets num_avg
+      self.auto_averaging() # sets num_avg
       # units and trigmode are the same as the PowerMeter defaults
+      self.units = self.ask("POWER_UNIT?")
+      self.trigmode = None
+      # use lowest sampling speed
+      self.ask("ACQ_SPEED 20")
       self.logger.info(" initialized %s", device[5:])
     else:
       self.logger.warning(" initialization failed")
@@ -139,6 +145,8 @@ class Radipower(PowerMeter,Serial):
   def set_cal_freq(self, freq=None):
     """
     Gets or sets the calibration frequency
+    
+    This is only useful when measuring CW signals, not noise
 
     @param freq : frequency in GHz
     @type  freq : float
@@ -157,15 +165,95 @@ class Radipower(PowerMeter,Serial):
     return self.f_cal
 
   def get_temp(self):
+    """
+    Reports the Radipower physical temperature.
+    
+    Power measurements will be interrupted if a temperature reading is
+    requested.
+    """
     self.temp = float(self.ask("TEMPERATURE?"))/10.
     self._add_attr("temp")
     return self.temp
+  
+  def get_read_speed(self):
+    """
+    Returns the time taken for one reading in s.
+    
+    On an RPi-1 this takes about 25 s at ACQ_SPEED=1000.
+    """
+    t0 = time()
+    for i in range(100):
+      x = self.ask('POWER?')
+    return (time()-t0)/100
 
+  def filter_bounds(self, samples_per_reading):
+    """
+    """
+    if samples_per_reading < 10:
+      bounds = (None,1)
+    elif samples_per_reading == 10:
+      bounds = (1,1)
+    elif samples_per_reading < 30:
+      bounds = (1,2)
+    elif samples_per_reading == 30:
+      bounds = (2,2)
+    elif samples_per_reading < 100:
+      bounds = (2,3)
+    elif samples_per_reading == 100:
+      bounds = (3,3)
+    elif samples_per_reading < 300:
+      bounds = (3,4)
+    elif samples_per_reading == 300:
+      bounds = (4,4)
+    elif samples_per_reading < 1000:
+      bounds = (4,5)
+    elif samples_per_reading == 1000:
+      bounds = (5,5)
+    elif samples_per_reading < 3000:
+      bounds = (5,6)
+    elif samples_per_reading == 3000:
+      bounds = (6,6)
+    elif samples_per_reading < 5000:
+      bounds = (6,7)
+    elif samples_per_reading == 5000:
+      bounds = (7,7)
+    else:
+      bounds = (7,None)
+    return bounds
+
+  def auto_averaging(self):
+    """
+    Power meter selects best avaraging based on signal level.
+    """
+    self.filter = "AUTO"
+    response  = self.ask("FILTER AUTO")
+    self.get_averaging()
+    return response
+    
+  def set_VBW(self,code):
+    """
+    Sets the video bandwidth
+    
+    VBW can be used to smooth the post-detection signal.  It sets the timescale
+    over which the measured signal varies.  VBW = 1/smoothing_time_constant.
+    
+    If an AM modulated signal is to be measured with a modulation frequency of,
+    for example, 1 kHz, the VBW should be set to 0 (10 MHz), 1 (1 MHz),
+    2 (200 kHz) or AUTO (0 at 1MSps, 1 at 100kSps, 2 at 20kSps). To measure the
+    modulation the VBW should be 10 times smaller than the RF carrier frequency
+    but higher than the modulation frequency.
+    """
+    pass
+    
   # --------------------- methods defined for PowerMeter ----------------------
   
   def power(self):
     """
-    Returns one (possible averaged) power reading in dBm
+    Returns one (possible averaged) power reading
+    
+    In mode 0 (RMS mode), a new power measurement is started after the "power?"
+    command has been given. Depending on the filter setting, the RadiPower 
+    performs the required number of measurements and returns the RMS value.
     """
     self.reading = float(self.ask("POWER?")[:-4])
     self.logger.debug("power: reading is %6.2f", self.reading)
@@ -184,51 +272,86 @@ class Radipower(PowerMeter,Serial):
     if self.filter == "AUTO":
       self.power()
       if self.reading > -20. and self.reading <= 10.:
-        self.num_avg = 10
-      elif self.reading > -30. and self.reading <= -20.:
-        self.num_avg = 30
-      elif self.reading > -40. and self.reading <= -30.:
         self.num_avg = 100
-      elif self.reading > -50. and self.reading <= -40.:
+      elif self.reading > -30. and self.reading <= -20.:
         self.num_avg = 300
-      else:
+      elif self.reading > -40. and self.reading <= -30.:
         self.num_avg = 1000
+      elif self.reading > -50. and self.reading <= -40.:
+        self.num_avg = 3000
+      else:
+        self.num_avg = 5000
     else:
       self.num_avg = Radipower.averages[int(self.filter)]
     if self.filter == "1":
-      self.num_avg = 1
-    elif self.filter == "2":
-      self.num_avg = 3
-    elif self.filter == "3":
       self.num_avg = 10
-    elif self.filter == "4":
+    elif self.filter == "2":
       self.num_avg = 30
-    elif self.filter == "5":
+    elif self.filter == "3":
       self.num_avg = 100
-    elif self.filter == "6":
+    elif self.filter == "4":
       self.num_avg = 300
-    elif self.filter == "7":
+    elif self.filter == "5":
       self.num_avg = 1000
+    elif self.filter == "6":
+      self.num_avg = 3000
+    elif self.filter == "7":
+      self.num_avg = 5000
     return self.num_avg
-
-  def set_averaging(self, num_averages):
+    
+  def set_averaging(self, num, no_smear=False, min_rms=False, most=False):
     """
-    Sets filter code for number of averages closest to requested
-
-    @param num_averages : 1-1000; 0 for AUTO
-    @type  num_averages : int
+    Selects the averaging option for power meter readings
+    
+    Sets the number of samples to average.  If no keyword argument is given,
+    the averaging option is the one which averages the number of samples which
+    is closest to num.
+    
+    The acquisition speed and filter should be set in such a way that at least
+    one full period of the modulation signal is measured. At 1Msps, the filter
+    should be set to 5 or higher, which results in 1000 or more samples. At
+    lower sampling speeds, for example 100ksps, the filter should be set to 3
+    or higher to measure at least one full period of the envelope signal. 
+    
+    @param num : number of samples to average; calculate if 0
+    @type  num : int
+    
+    @param no_smear : if True, num is the largest < reading_time/sampling_time
+    @type  no_smear : bool
+    
+    @param min_rms : if True, num is the smallest > reading_time/sampling_time
+    @type  min_rms : bool
+    
+    @param most: largest number of samples available
+    @type  most: bool
+    
+    @return num_averaged
     """
-    if num_averages > 1000:
+    if num > 5000:
       raise RadipowerError(num_averages,"averages is greater than 1000")
-    if num_averages <= 1000 and num_averages > 0:
-      self.filter = int(round(log(num_averages,3.15)))+1
-      response  = self.ask("FILTER "+str(self.filter))
-    elif num_averages == 0:
-      self.filter = "AUTO"
-      response  = self.ask("FILTER AUTO")
+    elif num > 0:
+      read_speed = get_read_speed() # ms
+      if read_speed < 1 :
+        response = self.ask("BAUD 3")           # 0.3 ms/sample
+      elif read_speed < 2:
+        response = self.ask("BAUD 2")           # 0.6 ms/sample
+      else:
+        response = self.ask("BAUD 1")           # 1.25 ms/sample
+    elif num == 0:
+      samples_per_read = read_speed*20
+      bounds = filter_bounds(samples_per_read)
+      if most:
+        response = self.ask("FILTER 7")
+      elif no_smear:
+        response = self.ask("FILTER "+str(bounds[0]))
+      elif min_rms:
+        response = self.ask("FILTER "+str(bounds[1]))
+      else:
+        response = self.ask("FILTER "+str(nearest_index(array(bounds), num)))
     self.get_averaging()
     return response
-
+    
+    
 class RP_array(dict):
   """
   Array of power meters
--- apps/tpr_server.py
+++ apps/tpr_server.py
@@ -2,10 +2,12 @@
 Dare! Total Power Radiometer server
 """
 import logging
+import sys
 
 from support import NamedClass, check_permission
-
-from support.pyro import initiate_server, PyroServer
+from support.logs import get_loglevel, initiate_option_parser, init_logging
+from support.logs import set_module_loggers
+from support.pyro import PyroServerLauncher, PyroServer
 from Electronics.Instruments.radiometer import Radiometer
 from Electronics.Instruments.Radipower import find_radipowers
 
@@ -14,7 +16,7 @@ module_logger = logging.getLogger(__name__)
 class RadiometerServer(PyroServer, Radiometer):
   """
   """
-  def __init__(self, name):
+  def __init__(self, name, rate=0.2):
     """
     """
     self.logger = logging.getLogger(module_logger.name+".RadiometerServer")
@@ -24,7 +26,7 @@ class RadiometerServer(PyroServer, Radiometer):
       raise RuntimeError("Insufficient permission to access USB")
     pm = find_radipowers()
     self.logger.debug(" initializing")
-    Radiometer.__init__(self, pm, rate=0.2)
+    Radiometer.__init__(self, pm, rate=rate)
     data = {}
     # dummy reading to wake up Radipower
     for key in pm.keys():
@@ -33,5 +35,28 @@ class RadiometerServer(PyroServer, Radiometer):
     self.run = True
 
 if __name__ == "__main__":
-  initiate_server(RadiometerServer, "radiometer_server")
 
+  def main(name):
+    """
+    """
+    p = initiate_option_parser(
+     """Generic Pyro server which servers as a template for actual servers.""")
+    # Add other options here
+  
+    opts, args = p.parse_args(sys.argv[1:])
+  
+    # This cannot be delegated to another module or class
+    mylogger = init_logging(logging.getLogger(),
+                            loglevel   = get_loglevel(opts.file_loglevel),
+                            consolevel = get_loglevel(opts.stderr_loglevel),
+                            logname    = opts.logpath+name)
+    mylogger.debug(" Handlers: %s", mylogger.handlers)
+    loggers = set_module_loggers(eval(opts.modloglevels))
+
+    psl = PyroServerLauncher(name+"Server") # the name by which the Pyro task is known
+    m = RadiometerServer(name) # identifier for the hardware
+    psl.start(m)
+  
+    psl.finish()
+  
+  main("Radiometer")
